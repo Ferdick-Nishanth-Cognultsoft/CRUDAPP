48f8e7645b8e2c52ecce836e63ce0996c3e8d29f
Ferdick-Nishanth-Cognultsoft <Fedrick@cognultsoft.com>
2022-02-01 13:47:39 +0530

    modified Style

--- CrudApplication/src/EditCustomers.tsx
+++ CrudApplication/src/EditCustomers.tsx
@@ -9,7 +9,7 @@ import { useFetch, usePost } from "./utility/apiHelper";
 
 const useStyles = makeStyles((theme) => ({
     root: {
-        flexGrow:  1, 
+        flexGrow:  1
     },
     menuButton: {
         marginRight: theme.spacing(2),
